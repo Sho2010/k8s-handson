1b04f3b315b1ff8a517b6962e59241904418aef5
sho naito <sho20100@gmail.com>
2017-03-30 14:28:56 +0900

    awesome image

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require "sinatra"
 
 get "/" do
-  "Hello World: #{ENV["MESSAGE"]}"
+  '<img src="http://serif.hatelabo.jp/images/cache/2b0a04a1636b501e0ad202e672039491b05690d9/54e818495185bad7f2108e64eb2fea297580264b.gif" alt="強い気持ち">'
 end
